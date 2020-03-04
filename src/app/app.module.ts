c4d7afc22d26a2d6f1bb0ca033aa09c90c90d900
The Buildbot <buildbot@angular2buch.de>
2020-03-04 22:41:17 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/angular-schule/book-monkey4-src/commit/116088f0615a9a5f8835653706a8733338376001
    CircleCI build: https://circleci.com/gh/angular-schule/book-monkey4-src/51

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component'; // NEW
+import { BookListComponent } from './book-list/book-list.component';
+import { BookListItemComponent } from './book-list-item/book-list-item.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BookListComponent,
+    BookListItemComponent,
+    BookDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
